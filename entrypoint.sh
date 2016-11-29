5109330c7921617f9841a6601827e40b2464bbba
13yo <tobias.kaatz@googlemail.com>
2016-11-29 12:07:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 if [ ! -f /cfssl/ca.pem ]
   then
     cd /cfssl
     cfssl genkey -initca /cfssl/ca.json | cfssljson -bare ca
 fi
 
-#!/usr/bin/env bash
 echo "Starting cfssl server"
 exec cfssl serve -address 0.0.0.0 -config /cfssl/config.json -ca /cfssl/ca.pem -ca-key /cfssl/ca-key.pem $@
