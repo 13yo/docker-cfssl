94af993778f5e81d27513ba9984acfe0950b08a1
13yo <tobias.kaatz@googlemail.com>
2016-12-02 11:59:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 if [ ! -f /cfssl/ca.pem ]
   then
     cd /cfssl
-    cfssl genkey -initca /cfssl/ca.json | cfssljson -bare ca
+    cfssl genkey -initca /opt/cfssl/ca.json | cfssljson -bare ca
 fi
 
 echo "Starting cfssl server"
-exec cfssl serve -address 0.0.0.0 -config /cfssl/config.json -ca /cfssl/ca.pem -ca-key /cfssl/ca-key.pem $@
+exec cfssl serve -address 0.0.0.0 -config /opt/cfssl/config.json -ca /cfssl/ca.pem -ca-key /cfssl/ca-key.pem $@
